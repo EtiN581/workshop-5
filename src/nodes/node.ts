4b6dcbdb1b62b16c0f2464d4e02b6698c739c8cf
etienne goury <etienne.goury@edu.devinci.fr>
2024-03-26 17:11:53 +0100

    update

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -24,13 +24,13 @@ export async function node(
     k: isFaulty ? null : 0
   }
 
-  let proposals: Map<number, Value[]> = new Map()
-  let votes: Map<number, Value[]> = new Map()
+  let proposals: Map<number, Value[]> = new Map();
+  let votes: Map<number, Value[]> = new Map();
 
   // TODO implement this
   // this route allows retrieving the current status of the node
   node.get("/status", (req, res) => {
-    if(isFaulty) {
+    if (isFaulty) {
       res.status(500).send("faulty");
     } else {
       res.status(200).send("live");
@@ -40,64 +40,63 @@ export async function node(
   // TODO implement this
   // this route allows the node to receive messages from other nodes
   node.post("/message", (req, res) => {
-    let {x, k, type} = req.body;
-    if (!isFaulty && !state.killed) {
-      if (type === "propose") {
-        if (!proposals.has(k)) proposals.set(k, []);
-        proposals.get(k)!.push(x);
-        let proposal = proposals.get(k)!;
-
-        if (proposal.length >= (N-F)) {
-          let counts = Count(proposal);
-          if (counts[0] >= (N/2)) x=0;
-          else if (counts[1] > (N/2)) x=1;
-          else x = "?"
-          SendToAll(x, k, "vote");
-        }
-
-      } else if (type === "vote") {
-        if (!votes.has(k)) {
-          votes.set(k, []);
-        }
-        votes.get(k)!.push(x);
-        let vote = votes.get(k)!;
-        if (vote.length >= (N-F)) {
-          let counts = Count(vote);
-          if (counts[0] >= F + 1) {
-            state.x = 0;
-            state.decided = true;
-          } else if (counts[1] >= F + 1) {
-            state.x = 1;
-            state.decided = true;
-          } else {
-            if (counts[0] > counts[1]) state.x = 0;
-            else if (counts[0] < counts[1]) state.x = 1;
-            else state.x = Math.random() > 0.5 ? 0 : 1;
-            state.k = k + 1;
-            SendToAll(state.x, state.k, "propose")
-          }
+    let { x, k, type } = req.body;
+    if (isFaulty || state.killed) {
+      res.status(500);
+      return;
+    }
+    if (type === "propose") {
+      updateMap(proposals, k, x);
+      let proposal = proposals.get(k)!;
+      if (proposal.length >= (N - F)) {
+        let counts = Count(proposal);
+        if (counts[0] >= (N / 2)) x = 0;
+        else if (counts[1] > (N / 2)) x = 1;
+        else x = "?";
+        SendToAll(x, k, "vote");
+      }
+    } else if (type === "vote") {
+      updateMap(votes, k, x)
+      let vote = votes.get(k)!;
+      if (vote.length >= (N - F)) {
+        let counts = Count(vote);
+        if (counts[0] >= F + 1) {
+          state.x = 0;
+          state.decided = true;
+        } else if (counts[1] >= F + 1) {
+          state.x = 1;
+          state.decided = true;
+        } else {
+          if (counts[0] > counts[1]) state.x = 0;
+          else if (counts[0] < counts[1]) state.x = 1;
+          else state.x = Math.random() > 0.5 ? 0 : 1;
+          state.k = k + 1;
+          SendToAll(state.x, state.k, "propose");
         }
       }
     }
-    res.status(200)
+    res.status(200).send('Message received');
   });
 
-  function SendToAll(x:Value, k:number|null, type:string) {
+  function SendToAll(x: Value, k: number | null, type: string) {
     for (let i = 0; i < N; i++) {
-      if (i !== nodeId) {
-        fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            x: x,
-            k: k,
-            type: type
-          }),
-        });
-      }
+      fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          x: x,
+          k: k,
+          type: type
+        }),
+      });
     }
+  }
+
+  function updateMap(array: Map<number, Value[]>, k:number, x:Value) {
+    if (array.has(k)) array.set(k, []);
+    array.get(k)!.push(x);
   }
 
   function Count(values: Value[]) {
@@ -111,26 +110,26 @@ export async function node(
   // TODO implement this
   // this route is used to start the consensus algorithm
   node.get("/start", async (req, res) => {
-    while(!nodesAreReady()) await delay(10);
-    if (!isFaulty) {
-      state.x = initialValue;
-      state.k = 1;
-      state.decided = false
-      SendToAll(state.x, state.k, "propose")
-    } else {
+    while (!nodesAreReady()) await delay(10);
+    if (isFaulty) {
       state.x = null;
       state.decided = null;
       state.k = null;
+      res.status(500).send("faulty");
+    } else {
+      state.x = initialValue;
+      state.k = 1;
+      state.decided = false;
+      SendToAll(state.x, state.k, "propose");
+      res.status(200).send("Algorithm started");
     }
-
-    res.status(200)
   });
 
   // TODO implement this
   // this route is used to stop the consensus algorithm
   node.get("/stop", async (req, res) => {
-    state.killed=true;
-    res.status(200).send("killed");
+    state.killed = true;
+    res.status(200).send("stopped");
   });
 
   // TODO implement this
